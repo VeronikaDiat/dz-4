14186dbaa6db92b3f68661853946d2dbdedf4950
VeronikaDiat <108459819+VeronikaDiat@users.noreply.github.com>
2024-05-15 15:48:57 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Second object = new Second();
+        int returnValue = object.printDZ();
+
+    }
+}
---
+++ Second.java
@@ -0,0 +1,8 @@
+public class Second {
+    static int myInt = 277;
+
+    public int  printDZ(){
+        System.out.println ("My int is"+ ' ' + myInt);
+        return myInt;
+    }
+}
